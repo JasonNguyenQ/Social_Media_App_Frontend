e18e0ca27dcf79c8c43686923f9ddbea398f7fc6
Master-AMW <lgnjasonn@gmail.com>
2025-03-23 19:19:31 -0700

    Added input validation on client side for user registration

--- components/Register/Fields.tsx
+++ components/Register/Fields.tsx
@@ -1,14 +1,20 @@
-export default function InputField({ name, type, placeholder, label, required=false, autocomplete="on" } : { 
+export default function InputField({ name, type, placeholder, label, required=false, autocomplete="on", issues=[] } : { 
     name: string, 
     type: string,
     placeholder?: string,
     label: string,
     required?: boolean,
-    autocomplete?: string
+    autocomplete?: string,
+    issues?: Array<string>
 }){
     return (
         <div className="input-field">
             <label htmlFor={name}>{label}</label>
+            <ul className="issue-container">
+                {issues.map((issue, index)=>{
+                    return <li className="issue" key={index}>{issue}</li>
+                })}
+            </ul>
             <input name={name} id={name} type={type} placeholder={placeholder} required={required} autoComplete={autocomplete}></input>
         </div>
     );
--- components/Register/SignUp.tsx
+++ components/Register/SignUp.tsx
@@ -1,38 +1,82 @@
 import { Helmet } from "react-helmet-async";
 import { APP_NAME } from "../../constants/globals";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import InputField from "./Fields";
+import { z } from "zod"
 import "./SignUp.css";
 
+const userInputSchema = z.object({
+	username: z.
+		string()
+		.refine(data=>!data.includes(" "), { message: "Username must NOT include spaces"}),
+
+	password: z
+		.string()
+		.min(8, { message: "Password MUST have 8 characters or more" })
+		.regex(/[A-Z]/, { message: "Password MUST include at least one capital letter"})
+		.regex(/[0-9]/, { message: "Password MUST include at least one digit"})
+		.regex(/[^a-zA-Z0-9]/, { message: "Password MUST include at least one special character"}),
+
+	confirmation: z
+		.string(),
+
+	firstName: z
+		.string()
+		.regex(/^[A-Z][a-z]*$/, { message: "First name MUST start with an uppercase and trail with lowercase English letters" }),
+
+	lastName: z
+		.string()
+		.regex(/^[A-Z][a-z]*$/, { message: "Last name MUST start with an uppercase and trail with lowercase English letters" }),
+
+	
+}).refine((data)=> 
+	data.password === data.confirmation, 
+	{ 
+		message: "Passwords do NOT match", 
+		path: ["confirmation"]
+	}
+)
+
 export default function SignUp() {
+	const [issueMessages, setIssueMessages] = useState<Record<string,Array<string>>>({})
 	const navigate = useNavigate();
 
 	async function Register(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 		const formData = new FormData(e.currentTarget);
 
-		const username = formData.get("username");
-		const password = formData.get("password");
-		const confirmPassword = formData.get("confirmation");
-		const firstName = formData.get("firstName");
-		const lastName = formData.get("lastName");
+		const dataEntries = {
+			username: formData.get("username"),
+			password: formData.get("password"),
+			confirmation: formData.get("confirmation"),
+			firstName: formData.get("firstName"),
+			lastName: formData.get("lastName")
+		}
 
-		if (password !== confirmPassword) return;
-
-		await fetch("http://localhost:3000/api/users/register", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				username: username,
-				password: password,
-				firstName: firstName,
-				lastName: lastName,
-			}),
-		});
-		navigate("/login");
+		const result = userInputSchema.safeParse(dataEntries);
+		if(result.success){
+			await fetch("http://localhost:3000/api/users/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(dataEntries),
+			});
+			navigate("/login");
+		}
+		else{
+			setIssueMessages(
+				result.error.issues.reduce((map: Record<string,Array<string>>, issue) => {
+					const key = issue.path[0]
+					if (!(key in map)) {
+					map[key] = [];
+					}
+					map[key].push(issue.message)
+					return map;
+				}, {})
+			)
+		}
 	}
 
 	return (
@@ -52,6 +96,7 @@ export default function SignUp() {
 						placeholder="e.g. John"
 						autocomplete="first-name"
 						required
+						issues={issueMessages["firstName"]}
 					/>
 					<InputField
 						name="lastName"
@@ -60,6 +105,7 @@ export default function SignUp() {
 						placeholder="e.g. Doe"
 						autocomplete="family-name"
 						required
+						issues={issueMessages["lastName"]}
 					/>
 				</fieldset>
 
@@ -72,6 +118,7 @@ export default function SignUp() {
 						placeholder="e.g. ChubbyRabbit029"
 						autocomplete="username"
 						required
+						issues={issueMessages["username"]}
 					/>
 					<InputField
 						name="password"
@@ -80,6 +127,7 @@ export default function SignUp() {
 						placeholder="Enter your password"
 						autocomplete="new-password"
 						required
+						issues={issueMessages["password"]}
 					/>
 					<InputField
 						name="confirmation"
@@ -88,6 +136,7 @@ export default function SignUp() {
 						autocomplete="new-password"
 						placeholder="Verify your password"
 						required
+						issues={issueMessages["confirmation"]}
 					/>
 				</fieldset>
 				<p>
