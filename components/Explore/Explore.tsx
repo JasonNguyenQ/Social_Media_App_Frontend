ff35f8c2ce8ea090db1b7c598d7de15f5447979d
Master-AMW <lgnjasonn@gmail.com>
2025-03-29 10:53:14 -0700

    Added basic UI mockup for first column of explore page

--- components/Explore/Explore.tsx
+++ components/Explore/Explore.tsx
@@ -1,35 +1,51 @@
 import { Helmet } from 'react-helmet-async';
 import Navbar from '../Navbar/Navbar';
+import { Link } from "react-router-dom"
 import { APP_NAME } from '../../constants/globals'
-import { useEffect, useState } from 'react';
-import { useThrottle } from '../../hooks/useThrottle';
+import Right_Arrow_Icon from "/right_arrow_icon.svg"
+import New_Tab_Icon from "/new_tab_icon.svg"
+import "./Explore.css"
 
 export default function Explore(){
-    const INTERVAL = 200
-    const [ position, setPosition ] = useThrottle<[number,number]>([0,0],INTERVAL)
-    const [ renders, setRenders ] = useState<number>(0)
-
-    useEffect(()=>{
-        const fn = (e: MouseEvent)=>{
-            setPosition([e.clientX,e.clientY], ()=>{setRenders((r)=>r+1)})
-        }
-        window.addEventListener('mousemove', fn)
-        return ()=>{
-            window.removeEventListener('mousemove', fn)
-        }
-    },[])
 
     return (
-        <>
+        <div id="explore-page">
             <Helmet>
                 <title>Explore | {APP_NAME}</title>
                 <meta name='description' content='Explore a Wide Variety of User Created Content' />
             </Helmet>
             <Navbar/>
-            <p>The useThrottle hook only allows one event to fire within a certain time frame to prevent tons of rerenders from happening due to an event firing multiple times in rapid succession such as the mousemove event</p>
-            <p>Current Interval: {INTERVAL/1000}s</p>
-            <div>Position: {position[0]}, {position[1]}</div>
-            <div>Number of Rerenders: {renders}</div>
-        </>
+            <div className="explore-container">
+                <ul className="actions">
+                    <li>
+                        Filters
+                        <img src={Right_Arrow_Icon}/>
+                        <div className="filters">
+                            Placeholder
+                        </div>
+                    </li>
+                    <li>
+                        <Link to={""} target='_blank'>
+                            How it works
+                            <img src={New_Tab_Icon}/>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={""} target='_blank'>
+                            Rules and Regulations
+                            <img src={New_Tab_Icon}/>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={""} target='_blank'>
+                            Report
+                            <img src={New_Tab_Icon}/>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="content-container">Placeholder</div>
+                <div>Placeholder</div>
+            </div>
+        </div>
     )
 }
