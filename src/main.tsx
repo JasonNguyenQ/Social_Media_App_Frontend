bfb1f5960342301f4e33c227eae857e24e460cc0
Master-AMW <lgnjasonn@gmail.com>
2025-04-07 13:07:35 -0700

    Implemented lazy loading of routes and code splitting

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { lazy, Suspense } from 'react'
 import "./index.css";
-import ProtectedRoutes from "./ProtectedRoutes.tsx";
-import Messages from "../components/Messages/Messages.tsx";
-import Explore from "../components/Explore/Explore.tsx";
-import SignUp from "../components/Register/SignUp.tsx";
-import Login from "../components/Register/Login.tsx";
-import ProfileRouter from "../components/Profile/ProfileRouter.tsx"
+
+const ProtectedRoutes = lazy(()=> import("./ProtectedRoutes.tsx"));
+const Messages = lazy(()=> import("../components/Messages/Messages.tsx"));
+const Explore = lazy(()=> import("../components/Explore/Explore.tsx"));
+const SignUp = lazy(()=> import("../components/Register/SignUp.tsx"));
+const Login = lazy(()=> import("../components/Register/Login.tsx"));
+const ProfileRouter = lazy(()=> import("../components/Profile/ProfileRouter.tsx"))
 
 const queryClient = new QueryClient();
 
@@ -16,17 +18,19 @@ createRoot(document.getElementById("root")!).render(
 	<HelmetProvider>
 		<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
-				<Routes>
-					<Route path="*" element={<Login />} />
-					<Route path="/register" element={<SignUp />} />
-					<Route path="/search/:userid" element={<ProfileRouter />} />
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path="*" element={<Login />} />
+						<Route path="/register" element={<SignUp />} />
+						<Route path="/search/:userid" element={<ProfileRouter />} />
 
-					<Route element={<ProtectedRoutes />}>
-						<Route path="/messages" element={<Messages />} />
-						<Route path="/explore" element={<Explore />} />
-						<Route path="/profile" element={<ProfileRouter/>} />
-					</Route>
-				</Routes>
+						<Route element={<ProtectedRoutes />}>
+							<Route path="/messages" element={<Messages />} />
+							<Route path="/explore" element={<Explore />} />
+							<Route path="/profile" element={<ProfileRouter/>} />
+						</Route>
+					</Routes>
+				</Suspense>
 			</BrowserRouter>
 		</QueryClientProvider>
 	</HelmetProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks (id) {
+          if (id.includes("node_modules")){
+            if (id.includes("react-dom")){
+              return "react-dom"
+            }
+            else if (id.includes("react")){
+              return "react"
+            }
+            else if (id.includes("socket.io")){
+              return "socket.io"
+            }
+            else if (id.includes("zod")){
+              return "zod"
+            }
+            return "vendor"
+          }
+        }
+      }
+    }
+  }
 })
