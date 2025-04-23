484ad8cdaf7cd8d079df9af7d546429c6e0d2b9c
Master-AMW <lgnjasonn@gmail.com>
2025-04-23 11:33:55 -0700

    Made useToggle hook able to take in an interval to limit number of toggles per interval

--- hooks/useToggle.tsx
+++ hooks/useToggle.tsx
@@ -1,6 +1,11 @@
-import { useReducer } from 'react'
+import { useThrottle } from './useThrottle'
 
-export function useToggle(initialValue: boolean): [boolean, ()=>void]{
-    const [ toggle, setToggle ] = useReducer((state)=>!state, initialValue)
+export function useToggle(initialValue: boolean, interval: number = 0): [boolean, ()=>void]{
+    const [ toggle, setState ] = useThrottle(initialValue, interval)
+
+    function setToggle(): void {
+        setState(!toggle)
+    }
+
     return [ toggle, setToggle ]
 }
